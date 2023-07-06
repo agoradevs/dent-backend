044beaf5328fd707d8826528ec995d371564f5d9
moisicmo <46501241+moisicmo@users.noreply.github.com>
2023-07-05 23:50:16 -0400

    first commit

---
+++ src/server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const { createServer } = require('http');
+const { dbConnection } = require('./database/config');
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.server = createServer(this.app);
+
+        this.paths = {
+            //rutas
+        };
+
+
+        // Conectar a base de datos
+        this.conectarDB();
+
+        // Middlewares
+        this.middlewares();
+
+        // Rutas de mi aplicación
+        this.routes();
+
+    }
+
+    async conectarDB() {
+        await dbConnection();
+    }
+
+
+    middlewares() {
+        this.app.use(express.json({ limit: '50mb' }));
+        // CORS
+        this.app.use(cors());
+
+        // Lectura y parseo del body
+        this.app.use(express.json());
+
+        // Directorio Público
+        const publicPath = path.resolve(__dirname, './../public');
+        this.app.use(express.static(publicPath));
+    }
+
+    routes() {
+        //auth
+        // this.app.use(this.paths.auth, require('./routes/auth.route'));
+        //rutas
+    }
+
+    listen() {
+        this.server.listen(this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port);
+        });
+    }
+}
+
+
+module.exports = Server;
